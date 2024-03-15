cbb5630152b2c6a7ed5be5469612adfa1ad07e5d
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-03-15 10:04:00 +0100

    Factor of 3 gain by db on my local system

--- sim_test.cpp
+++ sim_test.cpp
@@ -523,7 +523,7 @@ BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE(test_db)
 BOOST_AUTO_TEST_CASE(test_db_init)
 {
-    iter = 10000;
+    iter = 100000;
 }
 BOOST_AUTO_TEST_SUITE(test_db_scaling)
 BOOST_AUTO_TEST_CASE(test_db_scaling)
